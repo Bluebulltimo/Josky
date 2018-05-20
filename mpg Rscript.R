8fa02eaabeaa22a96439d8d33dd2fb33726c4fa5
Bluebull Timo <blitztim6@gmail.com>
2018-05-20 13:29:39 +0300

    Remove redundant files

--- mpg Rscript.R
+++
@@ -1,6 +0,0 @@
-library("ggplot2")
-View(ggplot2::mpg)
-#plot hwy versus displ with class as color
-ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy,color = class))
-#plot hwy versus cyl
-ggplot(data = mpg) + geom_point(mapping = aes(x = cyl, y = hwy))
